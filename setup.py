8ece6cb7643f398509766f05493978ba39256754
davemc84 <davemcox@gmail.com>
2016-02-21 03:45:10 +0000

    Add setup.py to initiate gitmoney CLI

---
+++ setup.py
@@ -0,0 +1,17 @@
+#The following needs to be run in the command line to add the gitmoney interface:
+#sudo pip3 install --editable .
+
+from setuptools import setup
+
+setup(
+    name='gitmoney',
+    version='0.1',
+    py_modules=['gitmoney'],
+        install_requires=[
+            'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        gitmoney=gitmoney:cli
+    ''',
+)
