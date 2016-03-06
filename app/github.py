8a0d4e72b664e68c75f7b6d361529b85de4a8461
Git <masonborda@gmail.com>
2016-03-06 04:48:38 +0000

    Import the github token from an environment variable

--- app/github.py
+++ app/github.py
@@ -9,6 +9,7 @@ from multisig_wallet import multisig_wallet
 config_path = os.path.dirname(os.path.realpath(__file__)) + '/../config/repos.json'
 repository = json.loads(io.open(config_path, 'r').read())
 repository_path = repository['path']
+ACCESS_TOKEN = os.environ['GITHUB_TOKEN']
 DEFAULT_WALLET_PATH = os.path.join(os.path.expanduser('~'),
                                    ".two1",
                                    "wallet",
@@ -54,7 +55,7 @@ class github(object):
         params = github._decorate_issue_params(issue_title, description)
 
         github_url = "https://api.github.com/repos/" + repository['path'] + "/issues"
-        headers = { "Authorization": "token " + repository['token'],
+        headers = { "Authorization": "token " + GITHUB_TOKEN,
                     "Content-Type": "application/json",
                     "Cache-Control": "no-cache, no-store, must-revalidate"
         }
@@ -81,7 +82,7 @@ class github(object):
     def get_address_from_issue(issue_number):
 
         github_url = "https://api.github.com/repos/" + repository['path'] + '/issues/' + issue_number
-        headers = { "Authorization": "token " + repository['token'], "Content-Type": "application/json" }
+        headers = { "Authorization": "token " + GITHUB_TOKEN, "Content-Type": "application/json" }
 
         # Setup the request
         req = urllib.request.Request(github_url, headers=headers)
